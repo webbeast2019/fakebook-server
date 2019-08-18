987effdb4396d4985acdb7723d8912a015b01d6c
yuvalbl <yuval.bar.levi@gmail.com>
2019-08-18 18:19:25 +0300

    03

--- routes/posts.js
+++ routes/posts.js
@@ -4,12 +4,19 @@ const dataService = require('../data/data.service');
 const multer = require('multer');
 
 // get file name based on file object and data next-entry-id
-const generatePostFileName = (file) => `post${dataService.getNextEntryId()}.${file.mimetype.split('/')[1]}`;
+const generatePostFileName = (file) => {
+  const name = file.originalname.includes('.') ? file.originalname.split('.')[0] : file.originalname;
+  const ext = file.mimetype.split('/')[1];
+  const hash = Math.random().toString(36).substr(2) + (+new Date).toString(36);
+  return `${name}_${hash}}.${ext}`;
+};
+let nextFileName;
 
 const storage = multer.diskStorage({
   destination: 'images/',
   filename: (req, file, cb) => {
-    cb(null, generatePostFileName(file))
+    nextFileName = generatePostFileName(file);
+    cb(null, nextFileName)
   }
 });
 const upload = multer({storage: storage});
@@ -27,7 +34,7 @@ router
   .post('/', upload.single('image'), function (req, res, next) {
     const entryData = req.body;
     if(req.file) {
-      entryData['image'] = generatePostFileName(req.file);  // add file name to post data
+      entryData['image'] = nextFileName;  // add file name to post data
     }
     const newEntry = dataService.createEntry(entryData);
     res.send(newEntry);
@@ -35,6 +42,9 @@ router
   .put('/:id', upload.single('image'), function (req, res, next) {
     const entryId = parseInt(req.params.id);
     const entryData = {...req.body, id: parseInt(req.body.id)};
+    if(req.file) {
+      entryData['image'] = nextFileName;  // add file name to post data
+    }
     if (entryData.id !== entryId) {
       res.status(400).end(); // bad request
     } else {
